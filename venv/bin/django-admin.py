975d3582d54ae16f95e23c60ef79b7e88f51bf6f
Henryk Krajinski <henryk.krajinski@open.ac.uk>
2019-06-18 16:40:31 +0100

    1st

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/hk228/Desktop/UDEMYcourse-FullStackDev/MovieRaterAPI/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
