a09d55b0ebe178cfd6ef6ffed908896f3ef96778
Henryk Krajinski <henryk.krajinski@open.ac.uk>
2019-06-18 17:42:57 +0100

    3rd

---
+++ api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from rest_framework import routers
+from django.conf.urls import include
+
+
+router = routers.DefaultRouter()
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
