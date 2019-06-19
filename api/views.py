730772e2df9011f8ade7c1fed515d3bfea23776f
Henryk Krajinski <henryk.krajinski@open.ac.uk>
2019-06-19 14:52:27 +0100

    commit after lesson 36: 'Custom method'

--- api/views.py
+++ api/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from .models import Movie, Rating
 from .serializers import MovieSerializer, RatingSerializer
 
@@ -8,6 +10,12 @@ from .serializers import MovieSerializer, RatingSerializer
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+
+    @action(detail=True, methods=['POST'])
+    # pk means primary key
+    def rate_movie(self, request, pk=None):
+        response = {'message': 'its working'}
+        return Response(response, status=status.HTTP_200_OK)
 
 
 class RatingViewSet(viewsets.ModelViewSet):
